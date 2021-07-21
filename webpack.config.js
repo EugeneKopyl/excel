80a4a617f940b4881126fffbfa35c54f1a30bd75
eugene.kopyl <eugene.kopyl@vrpconsulting.com>
2021-07-21 09:29:06 +0300

    Render table from template

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = (env, argv) => {
             extensions: ['.js'],
             alias: {
                 '@': path.resolve(__dirname, 'src'),
-                'core': path.resolve(__dirname, 'src', 'core')
+                '@core': path.resolve(__dirname, 'src', 'core')
             }
         },
         devServer: {
