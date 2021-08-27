5b0921f4ab19720b98ae066cdc86088eb8836c08
eugene.kopyl <eugene.kopyl@vrpconsulting.com>
2021-08-27 16:44:47 +0300

    Finish routing

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,48 +1,57 @@
-import {DomListener} from './DomListener';
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
     constructor($root, options = {}) {
         super($root, options.listeners)
         this.name = options.name || ''
-        this.store = options.store
-        this.subscribe = options.subscribe || []
         this.emitter = options.emitter
+        this.subscribe = options.subscribe || []
+        this.store = options.store
         this.unsubscribers = []
 
         this.prepare()
     }
 
+    // Настраивааем наш компонент до init
     prepare() {}
 
+    // Возвращает шаблон компонента
     toHTML() {
         return ''
     }
 
+    // Уведомляем слушателей про событие event
     $emit(event, ...args) {
-        const unsub = this.emitter.emit(event, ...args)
-        this.unsubscribers.push(unsub)
+        this.emitter.emit(event, ...args)
     }
 
+    // Подписываемся на событие event
     $on(event, fn) {
-        this.emitter.subscribe(event, fn)
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
     }
 
     $dispatch(action) {
         this.store.dispatch(action)
     }
 
+    // Сюда приходят только изменения по тем полям, на которые мы подписались
     storeChanged() {}
 
     isWatching(key) {
         return this.subscribe.includes(key)
     }
 
+    // Инициализируем компонент
+    // Добавляем DOM слушателей
     init() {
         this.initDomListeners()
     }
 
+    // Удаляем компонент
+    // Чистим слушатели
     destroy() {
-        this.removeDomListeners()
-        this.unsubscribers.forEach( (unsub) => unsub())
+        this.removeDOMListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 }
